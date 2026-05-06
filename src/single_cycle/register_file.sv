32f69871a509141d2f0632710b0f718bbf12c7ff
Anushivan <anushtheepan@gmail.com>
2026-05-06 15:25:36 -0400

    working on register file Day 1

---
+++ src/single_cycle/register_file.sv
@@ -0,0 +1,14 @@
+module register_file (
+input logic clk, 
+input logic writeEnable,
+input logic [4:0] rs1,
+input logic [4:0] rs2,
+input logic [4:0] rd,
+input logic [31:0] write_data,
+output logic [31:0] read_data1,
+output logic [31:0] read_data2
+);
+
+
+
+endmodule
