19d5614290a90f1b8830fcaa99e3f7d162f451fe
Anushivan <anushtheepan@gmail.com>
2026-05-07 23:56:01 -0400

    finished register file testbench

--- tb/tb_register_file.sv
+++ tb/tb_register_file.sv
@@ -1,9 +1,9 @@
-'timescale 1ns/1ps
+`timescale 1ns/1ps
 
 module tb_register_file;
 logic clk, we;
 logic [4:0] rs1, rs2, rd;
-logic [31:0] write_data, read_data1, read_data2
+logic [31:0] write_data, read_data1, read_data2;
 
 register_file dut (.*);
 
@@ -20,15 +20,28 @@ initial begin
     we = 0;
     rs1 = 5'd1;
     #1;
-    $display("Read x1 = %0d (expect 42)", read_data 1);
+    $display("Read x1 = %0d (expect 42)", read_data1);
 
     rd = 5'd0; write_data = 32'd99; we = 1;
     @(posedge clk); #1;
     we = 0;
     rs1 = 5'd0;
     #1;
-    
+    $display("Read x0 = %0d (expect 0)", read_data1);
 
-end
+    rd = 5'd2; write_data = 32'd10; we = 1;
+    @(posedge clk); #1;
+    rd = 5'd3; write_data = 32'd20; we = 1;
+    @(posedge clk); #1;
+    we = 0;
+    rs1 = 5'd2; rs2 = 5'd3;
+    #1;
+    $display("Read x2 = %0d (expected 10)", read_data1);
+    $display("Read x3 = %0d (expected 10)", read_data2);
+    
+    $finish;
+    end
+
+endmodule
 
 
