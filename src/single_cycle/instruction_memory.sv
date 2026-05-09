60fb27622cd734849bd76ce44e36458370edf603
Anushivan <anushtheepan@gmail.com>
2026-05-09 00:20:47 -0400

    Added instruction memory

---
+++ src/single_cycle/instruction_memory.sv
@@ -0,0 +1,17 @@
+module instruction_memory(
+input logic [31:0] address,
+output logic [31:0] instruction
+);
+
+logic [31:0] array [63:0];
+
+initial begin
+
+$readmemh("programs/test1.hex", array);
+
+end
+
+assign instruction = array[address];
+
+
+endmodule
